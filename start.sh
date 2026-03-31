95b5c6cf35bf5532642eaf73e3b0622d3f3fdab8
seyselein <ac145323@uni-stuttgart.de>
2026-03-31 18:18:04 +0200

    add start script

---
+++ start.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#SBATCH --job-name=de_opt
+#SBATCH --output=de_opt.out
+#SBATCH --time=00:25:00
+#SBATCH --nodes=2
+#SBATCH --ntasks-per-node=32
+#SBATCH --cpus-per-task=1
+#SBATCH --partition=dev_cpu_il
+##SBATCH --exclusive
+##SBATCH --dependency=singleton
+
+sh clean.sh
+source de
+ns_node=$SLURMD_NODENAME
+pyro5-ns -n $ns_node &
+for i in $(seq 1 $SLURM_NTASKS); do
+	srun -n 1 -N 1  python server.py $i $ns_node >server_log 2>&1&
+done
+python start_de.py $ns_node > log 2>&1
+
